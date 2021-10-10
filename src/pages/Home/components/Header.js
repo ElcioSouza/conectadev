e2ede643bb34a0629eb2dab7ffe80f01f66fb6ba
Elcio <elcio.monico@gmail.com>
2021-10-10 19:05:17 -0300

    Initial

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="header">
+        <div className="box_toolbar">
+            <div>
+                <a href="/">Conecta Dev - alteração</a>
+            </div>
+            <div>
+                <button>Novo Post</button>
+                <span>IMG1</span>
+                <span>IMG1</span>
+            </div>
+        </div>
+    </header>
+    )
+  }
+
+  export default Header;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.css';
+import Header from './components/Header';
+
+function Home() {
+    return (
+    <div>
+        <Header />
+        <main className="main">
+            <div className="navbar">Navbar</div>
+            <div className="feed">Feed</div>
+        </main>
+    </div>
+    )
+}
+
+export default Home;
